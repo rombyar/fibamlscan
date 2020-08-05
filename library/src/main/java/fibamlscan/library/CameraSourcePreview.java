dfd0ce47652be19eff1e46f8e713a1e658e9705c
Ahmad Romadhon <romadhon1byar@gmail.com>
2020-08-05 22:23:43 +0700

    Update CameraSourcePreview.java
    Fix issue full screen 
    Problem: https://github.com/googlesamples/android-vision/issues/23#issue-106010890
    Solution: https://github.com/googlesamples/android-vision/issues/23#issuecomment-203534913

--- library/src/main/java/fibamlscan/library/CameraSourcePreview.java
+++ library/src/main/java/fibamlscan/library/CameraSourcePreview.java
@@ -122,6 +122,64 @@ public class CameraSourcePreview extends ViewGroup {
   }
 
   @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        int previewWidth = 320;
+        int previewHeight = 240;
+        if (cameraSource != null) {
+            Size size = cameraSource.getPreviewSize();
+            if (size != null) {
+                previewWidth = size.getWidth();
+                previewHeight = size.getHeight();
+            }
+        }
+
+        // Swap width and height sizes when in portrait, since it will be rotated 90 degrees
+        if (isPortraitMode()) {
+            int tmp = previewWidth;
+            previewWidth = previewHeight;
+            previewHeight = tmp;
+        }
+
+        final int viewWidth = right - left;
+        final int viewHeight = bottom - top;
+
+        int childWidth;
+        int childHeight;
+        int childXOffset = 0;
+        int childYOffset = 0;
+        float widthRatio = (float) viewWidth / (float) previewWidth;
+        float heightRatio = (float) viewHeight / (float) previewHeight;
+
+        // To fill the view with the camera preview, while also preserving the correct aspect ratio,
+        // it is usually necessary to slightly oversize the child and to crop off portions along one
+        // of the dimensions.  We scale up based on the dimension requiring the most correction, and
+        // compute a crop offset for the other dimension.
+        if (widthRatio > heightRatio) {
+            childWidth = viewWidth;
+            childHeight = (int) ((float) previewHeight * widthRatio);
+            childYOffset = (childHeight - viewHeight) / 2;
+        } else {
+            childWidth = (int) ((float) previewWidth * heightRatio);
+            childHeight = viewHeight;
+            childXOffset = (childWidth - viewWidth) / 2;
+        }
+
+        for (int i = 0; i < getChildCount(); ++i) {
+            // One dimension will be cropped.  We shift child over or up by this offset and adjust
+            // the size to maintain the proper aspect ratio.
+            getChildAt(i).layout(
+                    -1 * childXOffset, -1 * childYOffset,
+                    childWidth - childXOffset, childHeight - childYOffset);
+        }
+
+        try {
+            startIfReady();
+        } catch (IOException e) {
+            Log.e(TAG, "Could not start camera source.", e);
+        }
+    }
+  
+  /*@Override
   protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
     int width = 380;
     int height = 300;
@@ -163,7 +221,7 @@ public class CameraSourcePreview extends ViewGroup {
     } catch (IOException e) {
       Log.e(TAG, "Could not start camera source.", e);
     }
-  }
+  }*/
 
   private boolean isPortraitMode() {
     int orientation = context.getResources().getConfiguration().orientation;
